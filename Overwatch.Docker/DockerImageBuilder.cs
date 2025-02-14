c263435e8aab42c82e511a57c40a858cd19e9edf
Phil Scrace <phil.scrace@aveva.com>
2025-02-14 17:17:13 +0000

    Build docker container  fixed

--- Overwatch.Docker/DockerImageBuilder.cs
+++ Overwatch.Docker/DockerImageBuilder.cs
@@ -1,47 +1,45 @@
 ﻿using Docker.DotNet;
 using Docker.DotNet.Models;
-using global::Docker.DotNet.Models;
-using global::Docker.DotNet;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 
-namespace Overwatch.Docker
+class DockerAutomation
 {
-    class DockerAutomation
+    public static async Task BuildDockerImageAsync(string dockerfilePath, string imageName)
     {
-        public static async Task BuildDockerImageAsync(string dockerfilePath, string imageName)
+        // Create a new DockerClient
+        using (var client = new DockerClientConfiguration(new Uri("unix:///var/run/docker.sock")).CreateClient())
         {
-            // Create a new DockerClient
-            using (var client = new DockerClientConfiguration(new Uri("unix:///var/run/docker.sock")).CreateClient())
+            // Read the Dockerfile as a stream
+            using (var fileStream = new FileStream(dockerfilePath, FileMode.Open, FileAccess.Read))
             {
                 // Set the build parameters
                 var buildParameters = new ImageBuildParameters
                 {
-                    Tags = new[] { imageName },
-                    Dockerfile = dockerfilePath
+                    Tags = new[] { imageName }
                 };
 
                 // Build the Docker image
-                var buildResult = await client.Images.BuildImageFromDockerfileAsync(new Uri(dockerfilePath), buildParameters);
+                var buildResult = await client.Images.BuildImageFromDockerfileAsync(fileStream, buildParameters);
 
-                // Read the result stream
-                using (var reader = new System.IO.StreamReader(buildResult))
+                // Read the result stream (output of the build process)
+                using (var reader = new StreamReader(buildResult))
                 {
-                    var output = reader.ReadToEnd();
+                    var output = await reader.ReadToEndAsync();
                     Console.WriteLine(output);
                 }
-
-                Console.WriteLine($"Docker image {imageName} built successfully.");
             }
-        }
 
-        public static async Task Main(string[] args)
-        {
-            string dockerfilePath = "path/to/Dockerfile";  // Path to your Dockerfile
-            string imageName = "my-custom-image";          // Name of the image you want to build
-
-            await BuildDockerImageAsync(dockerfilePath, imageName);
+            Console.WriteLine($"Docker image {imageName} built successfully.");
         }
     }
 
+    public static async Task Main(string[] args)
+    {
+        string dockerfilePath = "path/to/Dockerfile";  // Path to your Dockerfile
+        string imageName = "my-custom-image";          // Name of the image you want to build
+
+        await BuildDockerImageAsync(dockerfilePath, imageName);
+    }
 }
