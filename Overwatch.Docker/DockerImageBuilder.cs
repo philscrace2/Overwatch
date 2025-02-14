5c3e92df82e4b0e4e282d01c73ae0c1b3da6ac77
Phil Scrace <phil.scrace@aveva.com>
2025-02-14 17:11:10 +0000

    Add docker runner and builder helpers

---
+++ Overwatch.Docker/DockerImageBuilder.cs
@@ -0,0 +1,47 @@
+﻿using Docker.DotNet;
+using Docker.DotNet.Models;
+using global::Docker.DotNet.Models;
+using global::Docker.DotNet;
+using System;
+using System.Threading.Tasks;
+
+namespace Overwatch.Docker
+{
+    class DockerAutomation
+    {
+        public static async Task BuildDockerImageAsync(string dockerfilePath, string imageName)
+        {
+            // Create a new DockerClient
+            using (var client = new DockerClientConfiguration(new Uri("unix:///var/run/docker.sock")).CreateClient())
+            {
+                // Set the build parameters
+                var buildParameters = new ImageBuildParameters
+                {
+                    Tags = new[] { imageName },
+                    Dockerfile = dockerfilePath
+                };
+
+                // Build the Docker image
+                var buildResult = await client.Images.BuildImageFromDockerfileAsync(new Uri(dockerfilePath), buildParameters);
+
+                // Read the result stream
+                using (var reader = new System.IO.StreamReader(buildResult))
+                {
+                    var output = reader.ReadToEnd();
+                    Console.WriteLine(output);
+                }
+
+                Console.WriteLine($"Docker image {imageName} built successfully.");
+            }
+        }
+
+        public static async Task Main(string[] args)
+        {
+            string dockerfilePath = "path/to/Dockerfile";  // Path to your Dockerfile
+            string imageName = "my-custom-image";          // Name of the image you want to build
+
+            await BuildDockerImageAsync(dockerfilePath, imageName);
+        }
+    }
+
+}
---
+++ Overwatch.Docker/DockerTestRunner.cs
@@ -0,0 +1,38 @@
+﻿using Docker.DotNet;
+using Docker.DotNet.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Overwatch.Docker
+{
+    public class DockerTestRunner
+    {
+        private static async Task RunTestsInDocker()
+        {
+            var client = new DockerClientConfiguration(new Uri("unix:///var/run/docker.sock")).CreateClient();
+
+            // Create and start a new container
+            var container = await client.Containers.CreateContainerAsync(new CreateContainerParameters
+            {
+                Image = "mcr.microsoft.com/dotnet/core/sdk:3.1", // Choose the appropriate image
+                Cmd = new[] { "dotnet", "test" }, // Command to run the tests
+                Tty = true
+            });
+
+            await client.Containers.StartContainerAsync(container.ID, new ContainerStartParameters());
+
+            // Wait for container to finish and collect the results (simplified for illustration)
+            var logs = await client.Containers.GetContainerLogsAsync(container.ID, new ContainerLogsParameters { ShowStdout = true, ShowStderr = true });
+            Console.WriteLine(logs);
+
+            // Clean up container
+            await client.Containers.RemoveContainerAsync(container.ID, new ContainerRemoveParameters { Force = true });
+        }
+
+        public static void Main()
+        {
+            RunTestsInDocker().GetAwaiter().GetResult();
+        }
+    }
+
+}
